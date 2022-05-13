366f3dcb8bece4262e673d1b3ab747a1da34d3ff
Anup81 <anupdhakal81@gmail.com>
2022-05-13 22:49:54 +0545

    React Featured and Progress Bar Created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Widget from '../../components/widget/Widget'
+import Featured from '../../components/featured/Featured'
+import Chart from '../../components/chart/Chart'
 import './home.scss'
 const Home = () => {
   return (
@@ -14,6 +16,11 @@ const Home = () => {
             <Widget type="order" />
             <Widget type="earning" />
             <Widget type="balance" />
+          </div>
+
+          <div className="charts">
+            <Featured />
+            <Chart />
           </div>
         </div>
     </div>
